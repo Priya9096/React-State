48cad7e902ccd8c90209ff62daa9b5696422c4fc
Priya9096 <priyakhairate0000@gmail.com>
2025-03-28 10:02:40 +0530

    basics of react

---
+++ basics/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "./components/Counter";
+import Greet from "./components/Greet";
+import Home from "./Home";
+function App(){
+  return (
+    <>
+    <Home name = "Priya" />
+    <Home age = {33} />
+    <Greet />
+    <Counter />
+    </>
+  )
+}
+
+export default App;
